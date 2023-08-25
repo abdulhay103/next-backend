a643693fdadaa8e96abd1793fd38f22bd537d2a3
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-25 14:50:55 +0600

    form data done

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -1,18 +1,11 @@
 import { NextResponse } from "next/server";
 
-//GET method
-export async function GET(req, res) {
-  const { searchParams } = new URL(req.url);
-  const id = searchParams.get("id");
-
-  return NextResponse.json({ msg: id });
-}
 //POST method
 export async function POST(req, res) {
-  const { searchParams } = new URL(req.url);
-  const id = searchParams.get("id");
-  const name = searchParams.get("name");
-  const origin = searchParams.get("origin");
+  const reqBody = await req.formData();
+  let id = reqBody.get("id");
+  let name = reqBody.get("name");
+  let country = reqBody.get("country");
 
-  return NextResponse.json({ id: id, name: name, origin: origin });
+  return NextResponse.json({ id: id, name: name, country: country });
 }
