313bcd4ca1b5a182f8ad493d29ceea547d2965c9
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-25 14:56:34 +0600

    done

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -2,10 +2,16 @@ import { NextResponse } from "next/server";
 
 //POST method
 export async function POST(req, res) {
-  const reqBody = await req.formData();
-  let id = reqBody.get("id");
-  let name = reqBody.get("name");
-  let country = reqBody.get("country");
+  const reqBody = await req.json();
+  let id = reqBody["id"];
+  let name = reqBody["name"];
+  let country = reqBody["country"];
+  let color = reqBody["color"];
 
-  return NextResponse.json({ id: id, name: name, country: country });
+  return NextResponse.json({
+    id: id,
+    name: name,
+    country: country,
+    color: color,
+  });
 }
