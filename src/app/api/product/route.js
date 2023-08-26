f9cedf7f17ee5b510b7373965659c6622490999b
abdulhay103 <mdabdulhay95@gmail.comm>
2023-08-26 12:55:12 +0600

    cookie setup done

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -1,10 +1,7 @@
 import { NextResponse } from "next/server";
-import { headers } from "next/headers";
 
 //POST method
 export async function POST(req, res) {
-  const allHeaders = headers();
-  let token = allHeaders.get("Token");
-  let connection = allHeaders.get("Connection");
-  return NextResponse.json({ Token: token, Connection: connection });
+  let myCookie = req.cookies.get("myCookie");
+  return NextResponse.json({ Cookie: myCookie });
 }
