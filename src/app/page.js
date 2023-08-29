14e8768642ba9732af37a8f7ecfcffa68012e4fb
abdulhay103 <abdulhay.me@gmail.com>
2023-08-29 11:38:50 +0600

    ok

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,21 @@
+import { cookies } from "next/headers";
 import Link from "next/link";
+import Button from "./components/Button";
 
 export default function page() {
+  function setCookie() {
+    cookies().set({
+      name: "Theme",
+      value: "Dark",
+      path: "/",
+      httpOnly: true,
+    });
+  }
   return (
     <main>
       <h1>Set the cookie by cliking the button</h1>
-      <Link className="bg-sky-100 rounded px-4 py-2" href="#">
-        Set Cookie
-      </Link>
+      <Button handler={setCookie}>Set Cookie</Button>
+      {/* <button onClick={() => setCookie()}>Set Cookie</button> */}
     </main>
   );
 }
