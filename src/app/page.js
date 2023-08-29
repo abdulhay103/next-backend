ced83c8184df613efec69c9c88418dcbf9e9d3ff
abdulhay103 <abdulhay.me@gmail.com>
2023-08-29 10:28:47 +0600

    setuping

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 export default function page() {
   return (
     <main>
-      <h1>Home Page</h1>
-      <h1>Test Page</h1>
+      <h1>Set Cookie By clicking the button bellow</h1>
+      <button>ok</button>
     </main>
   );
 }
