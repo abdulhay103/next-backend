0c7a436e947b373c156b7dd07840f2e2c09173ca
abdulhay103 <abdulhay.me@gmail.com>
2023-08-29 11:52:44 +0600

    ok

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,7 @@
-import { cookies } from "next/headers";
-import Link from "next/link";
-import Button from "./components/Button";
-
 export default function page() {
-  function setCookie() {
-    cookies().set({
-      name: "Theme",
-      value: "Dark",
-      path: "/",
-      httpOnly: true,
-    });
-  }
   return (
     <main>
-      <h1>Set the cookie by cliking the button</h1>
-      <Button handler={setCookie}>Set Cookie</Button>
-      {/* <button onClick={() => setCookie()}>Set Cookie</button> */}
+      <h1>Set the cookie</h1>
     </main>
   );
 }
